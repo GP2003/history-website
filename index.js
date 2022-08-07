918ba967b2c4d2e389690417cd79d74f1eef54c0
palchurigeetha8@gmail.com <geethap2@illinois.edu>
2022-08-07 11:25:31 -0500

    added css folder and index.html and index.js files

---
+++ index.js
@@ -0,0 +1,18 @@
+$("#rockefellerImg").mouseover(function() {
+    $("#rockefellerImg").attr("src", "https://media.giphy.com/media/e7paKYobTt6hMIvQMt/giphy.gif");
+  })
+  
+  $("#rockefellerImg").on({
+      'mouseover': function(){
+          $("#rockefellerImg").attr('src',"https://media.giphy.com/media/e7paKYobTt6hMIvQMt/giphy.gif");
+      }
+  });
+  
+  $("#titanicImg").mouseover(function() {
+    $("#titanicImg").attr("src", "https://media.giphy.com/media/1eAg3lPj1dJYY/giphy.gif");
+  })
+  $("#titanicImg").on({
+      'mouseover': function(){
+          $("#titanicImg").attr('src',"https://media.giphy.com/media/1eAg3lPj1dJYY/giphy.gif");
+      }
+  });
